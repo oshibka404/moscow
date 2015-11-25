24feda4bea18ee30234a3fc244f1d12df4d03238
Andrey Ozornin <andrey.ozornin@team.wrike.com>
2015-11-25 03:56:20 +0300

    Refactoring

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -153,149 +153,205 @@ var M = {
 			})
 			.bins(31)(data);
 
-		var x = this.xScale = d3.scale.linear()
-			.domain([0, 31])
+		this.xScale = d3.scale.linear()
+			.domain([1, 30])
 			.range([0, width]);
 
-		var y = d3.scale.linear()
+		this.yScale = d3.scale.linear()
 			.domain([0, d3.max(centerData, function(d) { return d.y; })])
 			.range([height, 0]);
 
-		var histogram = this.svg.append('g')
-			.attr('transform', 'translate(' + (this.width - width) + ', 100)');
+		this.histogramContainer = this.svg.append('g')
+			.attr('transform', 'translate(' + (this.width - width - 20) + ', 100)')
+			.style('opacity', 0);
+		
+		this.histogramContainer.transition()
+				.duration(transDuration)
+				.style('opacity', .8);
 
-		var rectHeight = height * 2 + 20;
-		histogram.append('rect')
+		var rectHeight = height * 2 + 80;
+
+		this.histogramContainer.append('rect')
 			.attr('width', width)
 			.attr('height', rectHeight)
 			.attr('fill', '#c0c0c0')
-			.attr('stroke', '#000')
-			.style('opacity', .8);
+			.attr('stroke', '#000');
 
+		this.histogram = this.histogramContainer.append('g')
+			.attr('transform', 'translate(0, 10)');
+
+		this._drawChart(centerData, perefData, height);
+		this._appendDigits(width, height);
+		this._initDND(width, rectHeight, centerData);
+	},
+
+	_drawChart: function(centerData, perefData, height) {
+		var that = this;
 		var centerArea = d3.svg.area()
-			.x(function(d) { return x(d.x); })
+			.x(function(d) { return that.xScale(d.x); })
 			.y0(height)
-			.y1(function(d) { return y(d.y); });
+			.y1(function(d) { return that.yScale(d.y); });
 
-		histogram.append('path')
+		this.histogram.append('path')
 			.datum(centerData)
 			.attr('class', 'centerarea')
-			.attr('d', centerArea)
-			.style('opacity', 0)
-			.transition()
-				.duration(transDuration)
-				.style('opacity', 1);
+			.attr('d', centerArea);
 
 		var perefArea = d3.svg.area()
-			.x(function(d) { return x(d.x); })
+			.x(function(d) { return that.xScale(d.x); })
 			.y0(height)
-			.y1(function(d) { return height*2 - y(d.y); });
+			.y1(function(d) { return height*2 - that.yScale(d.y); });
 
-		histogram.append('path')
+		this.histogram.append('path')
 			.datum(perefData)
 			.attr('class', 'perefarea')
-			.attr('d', perefArea)
-			.style('opacity', 0)
-			.transition()
-				.duration(transDuration)
-				.style('opacity', 1);
+			.attr('d', perefArea);
+	},
 
+	_appendDigits: function(width, height) {
 		var fontSize = 60;
 
-		this.centerCount = histogram.append('text')
+		this.centerCount = this.histogram.append('text')
 			.text(d3.selectAll('.center')[0].length)
 			.attr('y', height / 2)
 			.attr('dx', width / 2)
 			.attr('dy', fontSize / 2)
 			.attr('text-anchor', 'middle')
 			.style('fill', '#000')
-			.style('font-size', fontSize)
-			.style('opacity', 0);
+			.style('font-size', fontSize);
 
-		this.centerCount.transition()
-			.duration(transDuration)
-			.style('opacity', 1);
-
-		this.perefCount = histogram.append('text')
+		this.perefCount = this.histogram.append('text')
 			.text(d3.selectAll('.peref')[0].length)
-			.attr('y', height * 1.5)
+			.attr('y', height * 1.3)
 			.attr('dx', width / 2)
 			.attr('dy', fontSize / 2)
 			.attr('text-anchor', 'middle')
 			.style('fill', '#000')
-			.style('opacity', 0)
-			.style('font-size', fontSize)
-		
-		this.perefCount.transition()
-			.duration(transDuration)
-			.style('opacity', 1);
+			.style('font-size', fontSize);
 
+		this.totalCount = this.histogram.append('text')
+			.text(d3.selectAll('circle')[0].length)
+			.attr('y', height * 2)
+			.attr('dx', width / 2)
+			.attr('dy', fontSize)
+			.attr('text-anchor', 'middle')
+			.style('fill', '#000')
+			.style('font-size', fontSize)
+	},
+
+	_initDND: function(width, height, centerData) {
 		var that = this;
-		var leftPosition = x.invert(0);
-		var rightPosition = x.invert(width);
+		var leftPosition = that.xScale.invert(0);
+		var rightPosition = that.xScale.invert(width);
 
 		var dragLeft = d3.behavior.drag()
 			.on('drag', function(d) {
-				var newPos = Math.max(x(1), Math.min(width-x(1), d3.event.x));
-				d.x = Math.floor(x.invert(newPos));
+				var newPos = Math.max(that.xScale(1), Math.min(width - that.xScale(1), d3.event.x));
+				d.x = Math.floor(that.xScale.invert(newPos));
 				d.x = (d.x < rightPosition) ? d.x : rightPosition-1;
 				d3.select(this)
-					.attr('transform', 'translate(' + x(d.x) + ')')
+					.attr('transform', 'translate(' + that.xScale(d.x) + ')')
 				leftPosition = d.x;
 				that._filterData(leftPosition, rightPosition);	
 			});
 
 		var dragRight = d3.behavior.drag()
 			.on('drag', function(d) {
-				var newPos = Math.max(x(1), Math.min(width-x(1), d3.event.x));
-				d.x = Math.ceil(x.invert(newPos));
+				var newPos = Math.max(that.xScale(1), Math.min(width - that.xScale(1), d3.event.x));
+				d.x = Math.ceil(that.xScale.invert(newPos));
 				d.x = (d.x > leftPosition) ? d.x : leftPosition+1;
 				d3.select(this)
-					.attr('transform', 'translate(' + x(d.x) + ')')
+					.attr('transform', 'translate(' + that.xScale(d.x) + ')')
 				rightPosition = d.x;
 				that._filterData(leftPosition, rightPosition);
 			});
 
-		histogram.append('polygon')
+		this.histogramContainer.append('polygon')
 			.data([{x: 1}])
-			.attr('points', '0,0 0,' + (rectHeight) + ' -10,' + (rectHeight) + ' 0,' + (rectHeight - 15))
+			.attr('points', '0,0 0,' + (height) + ' -10,' + (height) + ' 0,' + (height - 15))
 			.attr('transform', function(d) {
-				return 'translate(' + Math.floor(x(d.x)) + ')';
+				return 'translate(' + Math.floor(that.xScale(d.x)) + ')';
 			})
 			.style('stroke', 'black')
 			.style('fill', 'black')
 			.style('cursor', 'move')
 			.call(dragLeft);
 
-		histogram.append('polygon')
+		this.histogramContainer.append('polygon')
 			.data([{x: centerData.length-1}])
-			.attr('points', '0,0 0,' + (rectHeight) + ' 10,' + (rectHeight) + ' 0,' + (rectHeight - 15))
+			.attr('points', '0,0 0,' + (height) + ' 10,' + (height) + ' 0,' + (height - 15))
 			.attr('transform', function(d) {
-				return 'translate(' + Math.floor(x(d.x)) + ')';
+				return 'translate(' + Math.floor(that.xScale(d.x)) + ')';
 			})
 			.style('stroke', 'black')
 			.style('fill', 'black')
 			.style('cursor', 'move')
 			.call(dragRight);
+
+		this._appendDates(leftPosition, rightPosition, height);
+	},
+
+	_appendDates: function(startDate, finishDate, yPos) {
+		var fontSize = 15;
+
+		this.startDate = this.histogramContainer.append('text')
+			.text(startDate + ' Aug')
+			.attr('y', yPos)
+			.attr('dx', this.xScale(startDate))
+			.attr('dy', fontSize)
+			.attr('text-anchor', 'middle')
+			.style('fill', '#000')
+			.style('font-size', fontSize);
+
+		this.finishDate = this.histogramContainer.append('text')
+			.text(finishDate + ' Aug')
+			.attr('y', yPos)
+			.attr('dx', this.xScale(finishDate))
+			.attr('dy', fontSize)
+			.attr('text-anchor', 'middle')
+			.style('fill', '#000')
+			.style('font-size', fontSize);
 	},
 
 	_updateCount: function(center, peref, leftPosition, rightPosition) {
 		var fontSize = Math.max((rightPosition - leftPosition)*2, 15);
 		var dx = this.xScale(leftPosition) + (this.xScale(rightPosition) - this.xScale(leftPosition))/2;
+		var total = center + peref;
+
 		this.centerCount
 			.text(center)
 			.transition()
-			.duration(100)
-			.attr('dx', dx)
-			.attr('dy', fontSize/2)
-			.style('font-size', fontSize);
+				.duration(100)
+				.attr('dx', dx)
+				.attr('dy', fontSize/2)
+				.style('font-size', fontSize);
+		
 		this.perefCount
 			.text(peref)
 			.transition()
-			.duration(100)
-			.attr('dx', dx)
-			.attr('dy', fontSize/2)
-			.style('font-size', fontSize);
+				.duration(100)
+				.attr('dx', dx)
+				.attr('dy', fontSize/2)
+				.style('font-size', fontSize);
+
+		this.totalCount
+			.text(total)
+			.transition()
+				.duration(100)
+				.attr('dx', dx)
+				.style('font-size', fontSize);
+
+		this.startDate
+			.text(leftPosition + ' Aug')
+			.transition()
+				.duration(100)
+				.attr('dx', this.xScale(leftPosition));
+
+		this.finishDate
+			.text(rightPosition + ' Aug')
+			.transition()
+				.duration(100)
+				.attr('dx', this.xScale(rightPosition));
 
 	},
 
